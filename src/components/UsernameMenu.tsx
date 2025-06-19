76d2736f7cca937156c3e2bf9c9b31d4ec06694e
Mistura Raheem <olawunmiraheem@gmail.com>
2025-06-19 14:18:56 -0400

    Initial commit

---
+++ src/components/UsernameMenu.tsx
@@ -0,0 +1,41 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { CircleUserRound } from "lucide-react";
+import { DropdownMenuContent } from "./ui/dropdown-menu";
+import { Link } from "react-router-dom";
+import { Separator } from "@radix-ui/react-separator";
+import { Button } from "./ui/button";
+
+const UsernameMenu = () => {
+  const { user, logout } = useAuth0();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hober:text">
+        <CircleUserRound className="text-orange-500" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-orange-500">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem>
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UsernameMenu;
